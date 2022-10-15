b102bcc3b6ac4d0038b97dcb253c60e5b9d41ba4
cjpardodelahoz <cjpardodelahoz@gmail.com>
2022-10-15 14:15:36 -0400

    modified file with sample ids to match cleaned labels

--- commands.sh
+++ commands.sh
@@ -9,6 +9,9 @@ cat scripts/s8066.txt | sort | uniq > scripts/sample_ids_8066.txt
 # Merge and sort illumina reads from order 8066
 mkdir analyses/illumina/reads
 sbatch scripts/merge_illumina_reads_8066.sh
+# Modify sample id file to match the clenaed sample ids
+sed -i 's/-/_/' scripts/sample_ids_8066.txt
+sed -i 's/_punto/_top_qiagen/' scripts/sample_ids_8066.txt
 
 
 rsync -av scripts cjp47@dcc-login.oit.duke.edu:/hpc/group/bio1/cyanolichen_holobiome
