7bb3e2b403d5cd77a57cf7dee056808cb2f8d6db
Bedxxe <bedxxe@gmail.com>
2023-02-16 13:24:27 -0500

    Corrections on n1_top script

--- scripts/illumina/taxonomy/n1_top_taxonomy_kraken_bracken.sh
+++ scripts/illumina/taxonomy/n1_top_taxonomy_kraken_bracken.sh
@@ -24,7 +24,7 @@ b_reports=$(echo "/work/dg304/dala/cyanolichen_holobiome/analyses/illumina"/taxo
 source /hpc/group/bio1/diego/miniconda3/etc/profile.d/conda.sh
 conda activate metag
 # KRAKEN
-kraken2 --db /hpc/group/bio1/diego/programs/kraken2/01172023 --threads 24 --paired /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_all.fastq.gz         /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_all.fastq.gz         --output ${krakens}/${count}.kraken         --report ${k_reports}/${count}.report
+kraken2 --db /hpc/group/bio1/diego/programs/kraken2/01172023 --threads 24 --paired /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_paired.fq.gz         /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_paired.fq.gz         --output ${krakens}/${count}.kraken         --report ${k_reports}/${count}.report
 
 # BRACKEN
 bracken -d /hpc/group/bio1/diego/programs/kraken2/01172023         -i ${k_reports}/${count}.report         -o ${brackens}/${count}.bracken         -w ${b_reports}/${count}_bracken.report -r 150 -t 50
@@ -42,7 +42,7 @@ if [ 1 -eq 1 ];
         # Writting into .tsv file
         echo -e "Unclassified""\t""0" >> ${sequences}/${count}/otus_of_interest.tsv
         echo -e "\n""Extracting unclassified reads in fastq from sample: ""${count}"
-        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_all.fastq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_all.fastq.gz                 -o ${sequences}/${count}/unclassified_${count}_1.fq                 -o2 ${sequences}/${count}/unclassified_${count}_2.fq -t 0                 --fastq-output --include-children
+        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_paired.fq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_paired.fq.gz                 -o ${sequences}/${count}/unclassified_${count}_1.fq                 -o2 ${sequences}/${count}/unclassified_${count}_2.fq -t 0                 --fastq-output --include-children
     else
         echo "The user does not want to extract the unclassified reads"
 fi
@@ -55,7 +55,7 @@ if [ 1 -eq 1 ];
         # Writting into .tsv file
         echo -e "Non_bacterial""\t""2" >> ${sequences}/${count}/otus_of_interest.tsv
         echo -e "\n""Extracting non bacterial reads in fastq from sample: ""${count}"
-        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_all.fastq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_all.fastq.gz                 -o ${sequences}/${count}/non_bacterial_${count}_1.fq                 -o2 ${sequences}/${count}/non_bacterial_${count}_2.fq -t 2                 --fastq-output --include-children --exclude
+        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_paired.fq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_paired.fq.gz                 -o ${sequences}/${count}/non_bacterial_${count}_1.fq                 -o2 ${sequences}/${count}/non_bacterial_${count}_2.fq -t 2                 --fastq-output --include-children --exclude
     else
         echo "The user does not want to extract the non-Bacterial reads"
 fi
@@ -70,7 +70,7 @@ if [ "Nostoc" = "-" ];
         # Writting into .tsv file
         echo -e Nostoc"\t"${id_1_otu} >> ${sequences}/${count}/otus_of_interest.tsv
         echo -e "\n""Extracting ""Nostoc"" reads in fastq from sample: ""${count}"
-        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_all.fastq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_all.fastq.gz                 -o ${sequences}/${count}/Nostoc_${count}_1.fq                 -o2 ${sequences}/${count}/Nostoc_${count}_2.fq -t ${id_1_otu}                 --fastq-output --include-children
+        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_paired.fq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_paired.fq.gz                 -o ${sequences}/${count}/Nostoc_${count}_1.fq                 -o2 ${sequences}/${count}/Nostoc_${count}_2.fq -t ${id_1_otu}                 --fastq-output --include-children
         fi
 
 # 2nd READ EXTRACTION
@@ -83,7 +83,7 @@ if [ "Lecanoromycetes" = "-" ];
         # Writting into .tsv file
         echo -e Lecanoromycetes"\t"${id_2_otu} >> ${sequences}/${count}/otus_of_interest.tsv
         echo -e "\n""Extracting ""Lecanoromycetes"" reads in fastq from sample: ""${count}"
-        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_all.fastq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_all.fastq.gz                 -o ${sequences}/${count}/Lecanoromycetes_${count}_1.fq                 -o2 ${sequences}/${count}/Lecanoromycetes_${count}_2.fq -t ${id_2_otu}                 --fastq-output --include-children
+        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_paired.fq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_paired.fq.gz                 -o ${sequences}/${count}/Lecanoromycetes_${count}_1.fq                 -o2 ${sequences}/${count}/Lecanoromycetes_${count}_2.fq -t ${id_2_otu}                 --fastq-output --include-children
 fi
 
 # 3th READ EXTRACTION
@@ -96,4 +96,4 @@ if [ "Peltigera" = "-" ];
         # Writting into .tsv file
         echo -e Peltigera"\t"${id_3_otu} >> ${sequences}/${count}/otus_of_interest.tsv
         echo -e "\n""Extracting ""Peltigera"" reads in fastq from sample: ""${count}"
-        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_all.fastq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_all.fastq.gz                 -o ${sequences}/${count}/Peltigera_${count}_1.fq                 -o2 ${sequences}/${count}/Peltigera_${count}_2.fq -t ${id_3_otu}                 --fastq-output --include-children
+        extract_kraken_reads.py -k ${krakens}/${count}.kraken                 -r ${k_reports}/${count}.report                 -s1 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R1_paired.fq.gz                 -s2 /hpc/group/bio1/cyanolichen_holobiome/analyses/illumina/reads/${count}/${count}_R2_paired.fq.gz                 -o ${sequences}/${count}/Peltigera_${count}_1.fq                 -o2 ${sequences}/${count}/Peltigera_${count}_2.fq -t ${id_3_otu}                 --fastq-output --include-children
