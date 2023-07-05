3d54023421362f5585e8851f5ed9644549267ff5
cjpardodelahoz <cjpardodelahoz@gmail.com>
2023-07-05 15:50:07 -0400

    first complete draft of lifestyle tree, figure 1

--- scripts/phylogenetics/denovo/plot_astral_tree_busco1517.R
+++ scripts/phylogenetics/denovo/plot_astral_tree_busco1517.R
@@ -82,7 +82,7 @@ set_2_lifestyle <- set_2_raw_metadata %>%
   rename(c("taxon_name" = "specimen_id", "lifestyle_e" = "lifestyle")) %>%
   left_join(tip_labels_set2, by = "taxon_name") %>%
   select(tip_label, lifestyle_e) %>%
-  add_column(this_study = "thallus")
+  add_column(source = "thallus")
 
 # Env metagenomes (sets 5 and 6)
 # Env metagenomes tip labels
@@ -98,13 +98,15 @@ env_metagenomes_lifestyle <- env_metagenomes_raw_metadata %>%
   left_join(tip_labels_env_metagenomes, by = "taxon_name") %>%
   select(tip_label, lifestyle_e) %>%
   filter(!is.na(tip_label)) %>%
-  add_column(this_study = "env")
+  add_column(source = "env")
 # Merge all lifestyle tables and subset to tree labels
 all_sets_lifestyle <- bind_rows(set_1_lifestyle, set_11_lifestyle, 
                                 set_2_lifestyle, env_metagenomes_lifestyle) %>%
   right_join(tip_labels, by = "tip_label") %>%
-  select(tip_label, lifestyle_e, this_study) %>%
+  select(tip_label, lifestyle_e, source) %>%
   rename(lifestyle = lifestyle_e) %>%
+  mutate(source = 
+           replace_na(source, "public")) %>%
   distinct()
 
 ##### PLOT TREE WITH METADATA #####
@@ -119,10 +121,37 @@ tree$edge.length[is.na(tree$edge.length)] <- 1
 tree <- as_tibble(tree) %>%
   left_join(all_sets_lifestyle, by = c("label" = "tip_label")) %>%
   as.treedata()
-
-# Colors for categories
-lifestyle_colors <- c("azolla_symbiont" = )
-#
-ggtree(tree, layout="circular", branch.length = 'none') +
-  geom_tippoint(aes(color = factor(lifestyle), shape = factor(this_study))) +
-  scale_color_brewer(palette = "Set3")
+# Get node numbers for major clades
+subclade1_node <- MRCA(tree, c("Nostoc_sp_JC1668", "Nostoc_punctiforme_FACHB_252"))
+subclade2_node <- MRCA(tree, c("P12588_bin_4", "NOS_bin_1"))
+subclade3a_node <- MRCA(tree, c("P9820_bin_6", "P539_bin_14"))
+subclade3b_node <- MRCA(tree, c("P943_bin_5", "Nostoc_flagelliforme_FACHB_838"))
+# Shapes for source categories
+source_shapes <- c("public" = "circle", "thallus" = "cross", "env" = "triangle")
+# Colors for lifestyle categories
+lifestyle_colors <- c("azolla_symbiont" = "#A379B5", "bryophyte_associated" = "#E36A86",
+                      "cycad_symbiont" = "#F9C2D0", "Free-living" = "#C8F32F", "lichenized" = "#01A5CA")
+# Plot the tree with source and lifestyle metadata with the legend
+lifestyle_tree_withlegend_plot <- ggtree(tree, layout="circular", branch.length = 'none') +
+  geom_tippoint(aes(color = factor(lifestyle), shape = factor(source))) +
+  scale_color_brewer(palette = "Set3") +
+  geom_cladelab(node = subclade1_node, label = "subclade 1", 
+                angle = 290, offset= 0.5, offset.text= 1.4, hjust = 0.4, 
+                fontsize = 3.5, barsize = 0.8) +
+  geom_cladelab(node = subclade2_node, label = "subclade 2", 
+                angle = 353, offset= 0.5, offset.text= 1.4, hjust = 0.4, 
+                fontsize = 3.5, barsize = 0.8) +
+  geom_cladelab(node = subclade3a_node, label = "subclade 3a", 
+                angle = 85, offset= 0.5, offset.text= 1.4, hjust = 0.4, 
+                fontsize = 3.5, barsize = 0.8) +
+  geom_cladelab(node = subclade3b_node, label = "subclade 3b", 
+                angle = 20, offset= 0.5, offset.text= 1.4, hjust = 0.4, 
+                fontsize = 3.5, barsize = 0.8) +
+  scale_shape_manual(values = source_shapes) +
+  scale_color_manual(values = lifestyle_colors)
+# Plot the tree with source and lifestyle metadata without the legend
+lifestyle_tree_nolegend_plot <- tree_lifestyle_plot +
+  theme(legend.position = "none")
+# Save the tree plots
+ggsave(lifestyle_tree_nolegend_plot, filename = "documents/figures/lifestyle_tree_nolegend.pdf")
+ggsave(lifestyle_tree_withlegend_plot, filename = "documents/figures/lifestyle_tree_withlegend.pdf")
